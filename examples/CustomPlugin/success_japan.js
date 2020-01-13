de6594ea886c90ce5b198f833dad8ee0a6868ea2
hZieschank <hz@simplesurance.de>
2020-01-13 17:42:58 +0100

    Inital structure and old gist

---
+++ examples/CustomPlugin/success_japan.js
@@ -0,0 +1,37 @@
+var _sdbag = _sdbag || [];
+ 
+_sdbag.push(['partnerId', 404]); //replace it with your partner id
+_sdbag.push(['shopId', 9]); //replace it with your shop id
+_sdbag.push(['country', 'de']); //replace it with your customer's country code
+ 
+_sdbag.push(['orderId', '32432432432']); // replace it with the order id
+ 
+ // customer data
+_sdbag.push(['customer', {
+    firstname: "John",
+    lastname: "Smith",
+    email: "dadsadsda@example.com",
+    phone: "+49 (30) 00000000",
+    street: "Berliner str.",
+    street_number: 178,
+    zip: 10115,
+    city: "Berlin",
+    country: "de"
+}]);
+ 
+// COMMENT OUT IF YOU WANT TO WORK ON SANDBOX (PARTNER ID AND SHOP ID VALUES WILL BE DIFFERENT)
+//_sdbag.push(['sandbox', true]);
+ 
+// COMMENT OUT IF YOU WANT TO SEE DEBUG OUTPUT IN BROWSER'S JS CONSOLE. DO NOT ACTIVATE THIS IN PRODUCTION USE
+//_sdbag.push(['debug', true]);
+
+// This indicates which page you are initializing the plugin. Use "success" for success page.
+_sdbag.push(['page', 'success']);
+
+_sdbag.push(['init', 'success']);
+ 
+(function() {
+    var ss = document.createElement('script'); ss.type = 'text/javascript'; ss.async = true;
+    ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://')  + 'www.schutzklick.de/jsapi/sisu-checkout-2.x.min.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ss, s);
+})();
