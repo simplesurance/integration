0e3257d39b754c2794612a6091474bae1c198dc3
Lennard Bouma <55251692+schutzlenny@users.noreply.github.com>
2020-11-04 16:34:02 +0100

    Update checkout.js
    subdomain jsapi update https://sisu-agile.atlassian.net/browse/CORE-8047

--- checkout.js
+++ checkout.js
@@ -40,6 +40,6 @@ _sdbag.push(['init', 'checkout']); // When the "page" parameter set to "cart" or
  
 (function() {
     var ss = document.createElement('script'); ss.type = 'text/javascript'; ss.async = true;
-    ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://')  + 'www.schutzklick.de/jsapi/sisu-checkout-2.x.min.js';
+    ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://')  + 'jsapi.simplesurance.de/sisu-checkout-2.x.min.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ss, s);
 })();
